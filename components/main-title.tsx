1475b3b6a609648631cef070d880c48b7224d4c5
JPadley18 <jacob5180@hotmail.co.uk>
2023-03-26 00:39:56 +0000

    Use custom text blurring for chromatic aberration effect

--- components/main-title.tsx
+++ components/main-title.tsx
@@ -1,5 +1,5 @@
 export default function MainTitle({ text }: { text: string }) {
     return (
-        <h1 className="text-9xl text-center text-slate-300 text-shadow-lg font-mono">{text}</h1>
+        <h1 className="text-9xl text-center text-slate-300 font-mono [text-shadow:3px_3px_5px_#990000,-3px_-3px_5px_#000099]">{text}</h1>
     )
 }
