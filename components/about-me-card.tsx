7e286147c09fffc3d1e982722a3a77d6f827805b
JPadley18 <jacob5180@hotmail.co.uk>
2023-08-18 22:58:32 +0100

    Add mobile compatibility for About Me card

--- components/about-me-card.tsx
+++ components/about-me-card.tsx
@@ -10,11 +10,11 @@ const robotoMono = Roboto_Mono({
 
 export default function AboutMeCard() {
     return (
-        <div className="grid grid-cols-2 gap-32 justify-center mt-20">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-12 sm:gap-32 justify-center mt-20">
             <div>
-                <Image alt="Photo of myself" src="/me.png" width="256" height="256" className="rounded-full ml-auto" />
+                <Image alt="Photo of myself" src="/me.png" width="256" height="256" className="rounded-full mx-auto sm:ml-auto sm:mr-0" />
             </div>
-            <div className={robotoMono.className}>
+            <div className={`${robotoMono.className} pl-5 sm:pl-0`}>
                 <StatEntry text="Computer Science BSc (Hons)" icon={faGraduationCap} />
                 <StatEntry text="Royal Holloway University of London" icon={faBuildingColumns} />
                 <StatEntry text="JPadley18" icon={faGithub} />
