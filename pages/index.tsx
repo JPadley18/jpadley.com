c994432c727f4824d0bdb7046770182408574e04
JPadley18 <jacob5180@hotmail.co.uk>
2023-08-20 12:24:48 +0100

    Add content separator

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ export default function Home() {
         <div>
             <MainTitle text="JACOB PADLEY" />
             <AboutMeCard />
+            <hr className="my-48 mx-auto h-1 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-50 w-1/2" />
         </div>
     )
 }
