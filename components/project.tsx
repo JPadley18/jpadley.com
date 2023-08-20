e0dc63defa8c09f2217fc3d9739eae0d1ea2be47
JPadley18 <jacob5180@hotmail.co.uk>
2023-08-20 19:28:05 +0100

    Add language icons to project

--- components/project.tsx
+++ components/project.tsx
@@ -1,14 +1,26 @@
 import Image from "next/image"
+import Link from "next/link"
 import { robotoMono } from "@/util/fonts"
+import { IconDefinition } from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-export default function Project( { image, title, description, href }: { image: string, title: string, description: string, href: string } ) {
+export default function Project({ image, title, description, href, icons }: { image: string, title: string, description: string, href: string, icons: IconDefinition[] }) {
     return (
-        <div className="w-3/4 sm:w-1/3 bg-slate-900 border border-gray-200 shadow-lg shadow-gray-600 mx-auto mt-20 rounded-lg">
-            <Image src={image} alt="Project photo" width="256" height="256" className="object-cover w-full h-60 rounded-t-lg" />
-            <div className="p-5">
-                <h2 className={`${robotoMono.className} text-4xl font-bold`}>{title}</h2>
-                <p className={`${robotoMono.className} text-xl text-slate-400`}>{description}</p>
-            </div>
+        <div className="w-3/4 sm:w-1/3 bg-slate-900 border border-gray-200 shadow-lg hover:scale-105 transition-transform shadow-gray-600 mx-auto mt-20 rounded-lg">
+                <Link href={href}>
+                <Image src={image} alt="Project photo" width="256" height="256" className="object-cover w-full h-60 rounded-t-lg" />
+                <div className="p-5">
+                    <h2 className={`${robotoMono.className} text-3xl font-bold`}>{title}</h2>
+                    <p className={`${robotoMono.className} text-lg text-slate-400 pt-2`}>{description}</p>
+                    <ul className="pt-5">
+                        {icons.map((icon) => (
+                            <li key={icon.iconName} className="inline">
+                                <FontAwesomeIcon icon={icon} size="3x" className="pr-3" style={{color: "#FFFFFF"}} fixedWidth/>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </Link>
         </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import MainTitle from "@/components/main-title"
 import AboutMeCard from "@/components/about-me-card"
 import SubTitle from "@/components/sub-title"
 import Project from "@/components/project"
+import { faGolang, faJava, faPython } from "@fortawesome/free-brands-svg-icons"
 
 export default function Home() {
     return (
@@ -10,7 +11,7 @@ export default function Home() {
             <AboutMeCard />
             <hr className="my-16 sm:my-32 mx-auto h-1 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-50 w-1/2" />
             <SubTitle text="My Projects" />
-            <Project image="/me.png" title="Test Project" description="test" href="/" />
+            <Project image="/me.png" title="Test Project" description="test" href="/" icons={[faGolang, faJava, faPython]} />
         </div>
     )
 }
