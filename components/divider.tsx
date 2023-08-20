ff39e7a7642bb3978ba4466a08d1979ab07a585b
JPadley18 <jacob5180@hotmail.co.uk>
2023-08-20 20:46:03 +0100

    Make Divider its own component

---
+++ components/divider.tsx
@@ -0,0 +1,5 @@
+export default function Divider(){
+    return (
+        <hr className="my-16 sm:my-32 mx-auto h-1 border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-50 w-1/2" />
+    )
+}
