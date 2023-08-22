129277fcbb85c876fbdcd54d2a0e60a3bb358bc6
JPadley18 <jacob5180@hotmail.co.uk>
2023-08-22 17:40:02 +0100

    Improve responsiveness on main page

--- components/project.tsx
+++ components/project.tsx
@@ -6,7 +6,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function Project({ image, title, description, href, icons }: { image: string, title: string, description: string, href: string, icons: IconDefinition[] }) {
     return (
-        <div className="w-3/4 sm:w-1/3 bg-slate-900 border border-gray-200 shadow-lg hover:scale-105 transition-transform shadow-gray-600 mx-auto mt-20 rounded-lg">
+        <div className="w-3/4 lg:w-1/2 xl:w-1/3 bg-slate-900 border border-gray-200 shadow-lg hover:scale-105 transition-transform shadow-gray-600 mx-auto mt-20 rounded-lg">
                 <Link href={href}>
                 <Image src={image} alt="Project photo" width="256" height="256" className="object-cover w-full h-60 rounded-t-lg" />
                 <div className="p-5">
@@ -15,7 +15,7 @@ export default function Project({ image, title, description, href, icons }: { im
                     <ul className="pt-5">
                         {icons.map((icon) => (
                             <li key={icon.iconName} className="inline">
-                                <FontAwesomeIcon icon={icon} size="3x" className="pr-3" style={{color: "#FFFFFF"}} fixedWidth/>
+                                <FontAwesomeIcon icon={icon} size="2x" className="pr-3" style={{color: "#FFFFFF"}} fixedWidth/>
                             </li>
                         ))}
                     </ul>
