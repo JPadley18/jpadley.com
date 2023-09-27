c49cab76f2436a082ee4c802a673e044ff3033d9
JPadley18 <jacob5180@hotmail.co.uk>
2023-09-27 16:21:41 +0100

    Add placeholder Turtle project

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import AboutMeCard from "@/components/about-me-card"
 import SubTitle from "@/components/sub-title"
 import Project from "@/components/project"
 import Divider from "@/components/divider"
-import { faDocker, faGolang, faNodeJs } from "@fortawesome/free-brands-svg-icons"
+import { faDocker, faGolang, faNodeJs, faRaspberryPi, faPython } from "@fortawesome/free-brands-svg-icons"
 
 export default function Home() {
     return (
@@ -15,6 +15,9 @@ export default function Home() {
             <Project image="/cordle-thumb.png" width={934} height={684} title="Cordle"
             description="Competitive Wordle bot for Discord. Complete with an elo system so that you can prove that you're better than your friends once and for all."
             href="/cordle" icons={[faGolang, faDocker, faNodeJs]} />
+            <Project image="/cordle-thumb.png" width={934} height={684} title="Turtle Printer"
+            description="Software allowing new programmers to write Python code to draw images using the Turtle library and print their creations on a thermal receipt printer."
+            href="/turtle" icons={[faPython, faRaspberryPi]} />
         </div>
     )
 }
