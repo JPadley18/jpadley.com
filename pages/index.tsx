83831e5f4e9dca4c3e05a9d9fe0df903fe94c767
JPadley18 <jacob5180@hotmail.co.uk>
2023-09-28 11:13:02 +0100

    Add skeleton project entries for remaining projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import AboutMeCard from "@/components/about-me-card"
 import SubTitle from "@/components/sub-title"
 import Project from "@/components/project"
 import Divider from "@/components/divider"
-import { faDocker, faGolang, faNodeJs, faRaspberryPi, faPython } from "@fortawesome/free-brands-svg-icons"
+import { faDocker, faGolang, faNodeJs, faRaspberryPi, faPython, faJava, faAndroid } from "@fortawesome/free-brands-svg-icons"
 
 export default function Home() {
     return (
@@ -18,6 +18,15 @@ export default function Home() {
             <Project image="/cordle-thumb.png" width={934} height={684} title="Turtle Printer"
             description="Software allowing new programmers to write Python code to draw images using the Turtle library and print their creations on a thermal receipt printer."
             href="/turtle" icons={[faPython, faRaspberryPi]} />
+            <Project image="/cordle-thumb.png" width={934} height={684} title="TouCan"
+            description="Android app using OpenCV to identify a drinks can using the phone camera and send directions to a Lego Mindstorms robot to navigate to the can."
+            href="/toucan" icons={[faJava, faAndroid]} />
+            <Project image="/cordle-thumb.png" width={934} height={684} title="DataIker Recognition"
+            description="Software for the Raspberry Pi that uses machine learning and OpenCV to identify and greet office members by name using facial recognition."
+            href="/toucan" icons={[faPython, faRaspberryPi]} />
+            <Project image="/cordle-thumb.png" width={934} height={684} title="PyDrake"
+            description="Python wrapper for the League of Legends API."
+            href="/toucan" icons={[faPython]} />
         </div>
     )
 }
