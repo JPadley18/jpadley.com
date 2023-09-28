2c9edb4f5fcd43b88aa4b384095f90230c29ba2b
JPadley18 <jacob5180@hotmail.co.uk>
2023-09-28 12:49:01 +0100

    Add photos for turtle and toucan

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,10 +15,10 @@ export default function Home() {
             <Project image="/cordle-thumb.png" width={934} height={684} title="Cordle"
             description="Competitive Wordle bot for Discord. Complete with an elo system so that you can prove that you're better than your friends once and for all."
             href="/cordle" icons={[faGolang, faDocker, faNodeJs]} />
-            <Project image="/cordle-thumb.png" width={934} height={684} title="Turtle Printer"
+            <Project image="/turtle-thumb.jpg" width={1243} height={932} title="Turtle Printer"
             description="Software allowing new programmers to write Python code to draw images using the Turtle library and print their creations on a thermal receipt printer."
             href="/turtle" icons={[faPython, faRaspberryPi]} />
-            <Project image="/cordle-thumb.png" width={934} height={684} title="TouCan"
+            <Project image="/toucan-thumb.png" width={1920} height={864} title="TouCan"
             description="Android app using OpenCV to identify a drinks can using the phone camera and send directions to a Lego Mindstorms robot to navigate to the can."
             href="/toucan" icons={[faJava, faAndroid]} />
             <Project image="/cordle-thumb.png" width={934} height={684} title="DataIker Recognition"
